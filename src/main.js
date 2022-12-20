dc6f34c235452927697c20a0f47cdf45759fe374
Gwennie-zhou <Gwennie_zhou@126.com>
2022-12-20 19:08:45 +0800

    feat: 首页开发

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
+import '@/assets/style/reset.css'
+import '@/assets/style/common.css'
+
+
 import App from './App.vue'
 
 createApp(App).mount('#app')
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,73 @@
 <script setup>
-import { reactive } from 'vue'
 
-const state = reactive({ count: 0 })
-
-function increment() {
-  state.count++
-}
 </script>
 
 <template>
-  <button @click="increment">
-    {{ state.count }}
-  </button>
+  <div class="home-container">
+    <div class="bg-img"></div>
+    <img src="@/assets/images/me.jpg" alt="me" class="me-img" >
+    <div class="desc-wrap">
+      <div class="name">
+        <span>周蓉欢</span>
+        <span>Gwennie</span>
+      </div>
+      <div class="desc">
+        <div>Welcome to my world!</div>
+        <div>If you know me, you know my world.</div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style lang="less" scoped>
+  .home-container {
+    width: 100%;
+    height: 100vh;
+    .bg-img {
+      z-index: 2;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-image: url(../../assets/images/bg.png);
+      background-position: 50%;
+      background-repeat: no-repeat;
+      background-size: cover;
+    }
+    .me-img {
+      z-index: 1;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
+    .desc-wrap {
+      z-index: 3;
+      position: absolute;
+      bottom: 100px;
+      left: 150px;
+      font-family: cursive;
+      .name {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        width: 200px;
+        height: 200px;
+        border: 3px solid black;
+        background-color: white;
+        border-radius: 50%;
+        font-size: 35px;
+      }
+      .desc {
+        padding-top: 50px;
+        color: white;
+        font-size: 40px;
+        font-family: inherit;
+      }
+    }
+  }
+  
+</style>
