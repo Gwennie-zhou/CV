7ed6f10eef4ee638b9a17132285aa64217be1465
Gwennie-zhou <72902716+Gwennie-zhou@users.noreply.github.com>
2023-01-24 17:40:14 +0800

    feat: 个人信息模块增加总结页面

--- src/components/PersonalInfo.vue
+++ src/components/PersonalInfo.vue
@@ -26,15 +26,23 @@ let keywordsWrapDom = ''
 let showList = []
 let keywordDomList = undefined
 
+/* 个人信息模块动画入场顺序：
+1、出现引导语 -- Welcome to ...
+2、个人照片滚动入场
+3、个人照片和引导语消失
+4、进度条显示，关键字显示
+5、卡片出现，字体云出现
+
+*/
+
 onMounted(async () => {
-  createKeywordInfo()
+  // createKeywordInfo()
   await nextTick()
   keywordDomList = document.querySelectorAll('.keywords-wrap .keyword')
-  console.log("keywordDomList", keywordDomList);
   // animate()
   // running()
 
-  showSelfImg()
+  // showSelfImg()
   ScrollTrigger.create({
     trigger: '.per-info-container',
     start: 'top 20%',
@@ -57,7 +65,36 @@ onMounted(async () => {
     },
   })
 
-}) 
+  entranceAnimate()
+})
+
+// 字体云出场方式 先中间出现，再移向左边，然后卡片和段落再由下往上入场
+const entranceAnimate = () => {
+  console.log("entranceAnimate");
+  const t1 = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.conclusion',
+      start: 'top top',
+      scrub: 0.1,
+      pin: '.per-info-container',
+      markers: true
+    }
+  })
+  t1.to('.wordcloud-wrap', {
+    scale: 1,
+    duration: 2
+  })
+  t1.to('.wordcloud-wrap', {
+    x: '-100%',
+    duration: 2,
+    ease: "power2.out"
+  })
+  t1.from('.conclusion-right', {
+    y: '110%',
+    duration: 3
+  })
+}
+
 // 图片滚入场
 const showSelfImg = () => {
   const sections = document.querySelectorAll('.self-img')
@@ -111,7 +148,7 @@ const running = () => {
 
 <template>
   <div class="per-info-container">
-    <div class="guide">
+    <!-- <div class="guide">
       <div class="headline-wrap">
         <div class="headline">Welcome to the personal information module!</div>
         <div class="rectangle"></div>
@@ -136,25 +173,38 @@ const running = () => {
           :style="{'top': `${item.top}px`, 'left': `${item.left}px`, 'transform': `scale(${item.scale})`, 'filter': `blur(${item.blur}px)`}"
           >{{ item.keyword }}</span>
       </div>
-    </div>
-
-    <!-- <div class="card-wrap">
-      <img src="@/assets/images/card.jpg" alt="" class="card-img">
-      <div class="info">
-        <div class="left">
-          <div class="name">周蓉欢</div>
-          <div>前端开发工程师</div>
-        </div>
-        <ul class="right">
-          <li>出生年月：1998.06</li>
-          <li>目前居住地：深圳</li>
-          <li>学历：本科（全日制）</li>
-          <li>就读于：韩山师范学院</li>
-          <li>入学年份：2017-2021</li>
-          <li>专业：信息管理与信息系统</li>
-        </ul>
-      </div>
     </div> -->
+
+    <div class="conclusion">
+      <div class="wordcloud-wrap">
+        <img src="@/assets/images/wordcloud.png" alt="字体云">
+      </div>
+      <div class="conclusion-right">
+        <div class="text">
+          <p>Do you know me </p>
+          <p style="margin-left: 80px">a bit now ?</p>
+        </div>
+        <div class="card-wrap">
+          <img src="@/assets/images/card.jpg" alt="卡片背景" class="card-img">
+          <div class="info">
+            <div class="left">
+              <div class="name">周蓉欢</div>
+              <div>前端开发工程师</div>
+            </div>
+            <ul class="right">
+              <li>出生年月：1998.06</li>
+              <li>目前居住地：深圳</li>
+              <li>学历：本科（全日制）</li>
+              <li>就读于：韩山师范学院</li>
+              <li>入学年份：2017-2021</li>
+              <li>专业：信息管理与信息系统</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
   </div>
 </template>
 
@@ -166,6 +216,7 @@ const running = () => {
   background-size: cover;
   width: 100%;
   height: 100vh;
+
   .guide {
     position: absolute;
     top: 60%;
@@ -177,6 +228,7 @@ const running = () => {
     align-items: center;
     width: 60%;
     height: 60%;
+
     .scroll {
       width: 100px;
       height: 100px;
@@ -189,6 +241,7 @@ const running = () => {
       margin-top: 200px;
     }
   }
+
   .headline-wrap {
     .headline {
       color: white;
@@ -196,6 +249,7 @@ const running = () => {
       text-align: center;
       margin-bottom: 10px;
     }
+
     .rectangle {
       width: 60vw;
       height: 15px;
@@ -203,10 +257,12 @@ const running = () => {
       transform: scaleX(0);
       transition: transform 0.3s cubic-bezier(0.25, 1, 0.5, 1);
     }
+
     .extend {
       transform: scaleX(1); //scaleX将会从原点向两边扭曲一定程度
     }
   }
+
   .self-img-wrap {
     position: absolute;
     top: 0;
@@ -215,17 +271,20 @@ const running = () => {
     display: flex;
     width: 100%;
     height: 100%;
+
     .self-img {
       width: 33.33%;
-      object-fit: contain;
+      object-fit: cover;
     }
   }
+
   .keywords-wrap {
     position: absolute;
     width: 100%;
     height: 100%;
     top: 0;
     left: 0;
+
     .progress-wrap {
       position: absolute;
       top: 20%;
@@ -237,6 +296,7 @@ const running = () => {
       border-radius: 15px;
       overflow: hidden;
       display: none;
+
       .progress {
         position: absolute;
         z-index: 2;
@@ -246,6 +306,7 @@ const running = () => {
         border-radius: 15px;
         text-align: center;
       }
+
       .bar {
         position: absolute;
         z-index: 1;
@@ -256,6 +317,7 @@ const running = () => {
         background: red;
       }
     }
+
     .keyword {
       position: absolute;
       color: #00d67a;
@@ -263,38 +325,92 @@ const running = () => {
       width: 100px;
     }
   }
-  .card-wrap {
+
+  .conclusion {
     position: relative;
-    box-shadow: 0 0 20px 0px #00d67a;
-    width: 650px;
-    height: 400px;
-    .info {
+    width: 100%;
+    height: 100%;
+
+    .wordcloud-wrap {
       position: absolute;
-      top: 30%;
-      left: 15%;
-      z-index: 10;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      .left {
-        margin-right: 50px;
-        border-right: 1px solid white;
-        padding-right: 40px;
-        .name {
-          font-size: 30px;
-          padding-bottom: 10px;
-        }
+      bottom: 0%;
+      left: 40%;
+      width: 550px;
+      height: 800px;
+      transform: scale(0);
+
+      img {
+        width: 100%;
+        height: 100%;
       }
-      .right {
-        font-size: 22px;
-        li {
-          padding-bottom: 8px;
+    }
+
+    .conclusion-right {
+      position: absolute;
+      top: 15%;
+      right: 10%;
+      width: 700px;
+      height: 700px;
+
+      .text {
+        font-size: 80px;
+        text-shadow: 10px 10px 10px black;
+        margin-bottom: 100px;
+      }
+
+
+      .card-wrap {
+        position: absolute;
+        bottom: 10%;
+        right: 25%;
+        box-shadow: 0 0 20px 0px #00d67a;
+        width: 500px;
+        height: 300px;
+
+        .card-img {
+          width: 100%;
+          height: 100%;
+        }
+
+        .info {
+          position: absolute;
+          top: 22%;
+          left: 10%;
+          z-index: 10;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+
+          .left {
+            margin-right: 40px;
+            border-right: 1px solid white;
+            padding-right: 30px;
+
+            .name {
+              font-size: 30px;
+              padding-bottom: 10px;
+            }
+          }
+
+          .right {
+            font-size: 18px;
+
+            li {
+              padding-bottom: 8px;
+            }
+          }
         }
       }
     }
+
+
+
+
   }
-  
+
+
 }
+
 .active {
   transform: scale(5) !important;
   filter: blur(3px);
