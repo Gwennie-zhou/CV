2245e664da64389c1625043f166b2a9cadf9b2eb
Gwennie-zhou <72902716+Gwennie-zhou@users.noreply.github.com>
2023-01-28 13:55:52 +0800

    优化：优化个人技能模块UI页面，同时添加入场动画

--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -2,6 +2,11 @@
 import * as echarts from 'echarts';
 import { onMounted, reactive } from 'vue';
 
+// 动画库
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger.js";
+gsap.registerPlugin(ScrollTrigger);
+
 const skills = reactive([
   '熟练使用 Vue2+Vue3 全家桶开发项目',
   '熟练掌握 HTML5+CSS3 模式的 HTML 代码编写，熟练手写标准 CSS 样式',
@@ -15,215 +20,73 @@ const skills = reactive([
   '具有优秀的英语读写能力（英语六级），在 medium（需翻墙）上面写技术博客'
 ])
 
-const piesTop = reactive(['vue', 'react', 'js'])
-const piesBottom = reactive(['css', 'webpack'])
-
-
-function makeCharts() {
-  const vuePie = echarts.init(document.getElementById('vue_pie'));
-  const reactPie = echarts.init(document.getElementById('react_pie'));
-  const jsPie = echarts.init(document.getElementById('js_pie'));
-  const cssPie = echarts.init(document.getElementById('css_pie'));
-  const webpackPie = echarts.init(document.getElementById('webpack_pie'));
-
-
-  const commonOption = {
-    type: 'pie',
-    radius: ['50%', '70%'],
-    label: {
-      show: false,
-      position: 'center'
-    },
-    labelLine: {
-      show: false
-    },
-    color: [
-      '#00d67a',
-      'gray'
-    ],
-  }
-
-  vuePie.setOption({
-    title: {
-      text: '90%',
-      left: 'center',
-      top: 'center'
-    },
-    series: [
-      {
-        ...commonOption,
-        data: [
-          { value: 90, name: 'vue' },
-          { value: 10, name: 'other' }
-        ]
-      }
-    ]
+const animate = () => {
+  const animation = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.skill-container',
+      start: 'top 20%',
+      toggleActions: 'restart none none none'
+    }
   })
-
-  reactPie.setOption({
-    title: {
-      text: '50%',
-      left: 'center',
-      top: 'center'
-    },
-    series: [
-      {
-        ...commonOption,
-        data: [
-          { value: 50, name: 'react' },
-          { value: 50, name: 'other' }
-        ]
-      }
-    ]
-  })
-
-  jsPie.setOption({
-    title: {
-      text: '90%',
-      left: 'center',
-      top: 'center'
-    },
-    series: [
-      {
-        ...commonOption,
-        data: [
-          { value: 90, name: 'js' },
-          { value: 10, name: 'other' }
-        ]
-      }
-    ]
-  })
-
-  cssPie.setOption({
-    title: {
-      text: '90%',
-      left: 'center',
-      top: 'center'
-    },
-    series: [
-      {
-        ...commonOption,
-        data: [
-          { value: 90, name: 'css' },
-          { value: 10, name: 'other' }
-        ]
-      }
-    ]
-  })
-
-  webpackPie.setOption({
-    title: {
-      text: '65%',
-      left: 'center',
-      top: 'center'
-    },
-    series: [
-      {
-        ...commonOption,
-        data: [
-          { value: 65, name: 'webpack' },
-          { value: 35, name: 'other' }
-        ]
-      }
-    ]
-  })
-
+  animation
+    .to('.skill-container-box', { backgroundImage: 'linear-gradient(370deg, #00ff91 20%, black 20%)', duration: 1.5, ease: 'power4.out'})
+    .from('.skill-container .text', { opacity: 0, duration: 0.5})
+    .from('.skills ol li', {xPercent: 50, opacity: 0, duration: 0.5, stagger: 0.2})
 }
-
 onMounted(() => {
-  makeCharts()
+  animate()
 })
 
 </script>
 
 <template>
   <div class="skill-container">
-    <div class="left">
-      <ol>
-        <li v-for="(item, i) in skills" :key="i">
-          <span class="check">✔</span>
-          <span>{{ item }}</span>
-        </li>
-      </ol>
-    </div>
-    <div class="vl"></div>
-    <div class="right">
-      <div class="title">
-        <div>&lt;skill</div>
-        <div>/&gt;</div>
+    <div class="skill-container-box">
+      <div class="skills">
+        <ol>
+          <li v-for="(item, i) in skills" :key="i">
+            <span class="check">✔</span>
+            <span>{{ item }}</span>
+          </li>
+        </ol>
       </div>
-      <div class="charts">
-        <div class="top">
-          <div class="chart-wrap" v-for="(item, i) in piesTop" :key="i">
-            <div :id="item + '_pie'" class="pie"></div>
-            <div>{{ item }}</div>
-          </div>
-        </div>
-        <div class="bottom">
-          <div class="chart-wrap" v-for="(item, i) in piesBottom" :key="i">
-            <div :id="item + '_pie'" class="pie"></div>
-            <div>{{ item }}</div>
-          </div>
-        </div>
-      </div>
+      <div class="text">SKILL</div>
     </div>
+
   </div>
 </template>
 
 <style lang="less" scoped>
 .skill-container {
-  display: flex;
   width: 100%;
-  height: 50vh;
-  .left {
-    flex: 1;
-    padding: 50px 30px;
-    font-size: 20px;
+  height: 100vh;
+  .skill-container-box {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+
+  .skills {
+    font-size: 25px;
+    margin-left: 150px;
+    padding-top: 100px;
     ol li {
-      padding-bottom: 5px;
+      padding-bottom: 10px;
     }
+
     .check {
       margin-right: 5px;
       color: #00d67a;
     }
   }
 
-  .right {
-    width: 40%;
-    height: 100%;
-    padding: 20px 30px;
+  .text {
+    position: absolute;
+    left: 50px;
+    bottom: 50px;
+    letter-spacing: 0.18em;
+    font-size: 60px;
   }
 
-  .charts {
-    width: 80%;
-    height: 80%;
-    background: white;
-    color: black;
-    border-radius: 10px;
-
-    .chart-wrap {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      margin-right: 10px;
-      font-size: 20px;
-    }
-
-    .pie {
-      width: 120px;
-      height: 120px;
-    }
-
-    .top,
-    .bottom {
-      display: flex;
-      justify-content: center;
-    }
-
-    .top {
-      margin-bottom: 20px;
-    }
-  }
 }
 </style>
